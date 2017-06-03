b2578cfe6a20e3411630007d1d63f2c5e3408e5d
toannc <toan.nguyen-chi@lazada.com>
2017-06-03 21:06:09 +0700

    Improve testing code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 ssh prod@ch.ilawyer.vn <<EOF
-ssh root@localhost -p 2210 
-echo "Testing code from Production Branch"
-cd /opt/production
-git checkout master && git pull
-sh /nodejs/bin/npm install
-cd /opt/production/node_modules/mocha
-./bin/mocha /opt//production/test/test.js
+sudo docker exec 133e18e9d894 /bin/bash -c 'cd /opt/production; git checkout master; git pull; npm install; cd /opt/production; node_modules/mocha/bin/mocha /opt/production/test/test.js'
 EOF
