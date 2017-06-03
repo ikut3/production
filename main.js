eed3c9f49d9c236a5f441439d23847ffd07f5666
toannc <toan.nguyen-chi@lazada.com>
2017-06-03 10:06:58 +0700

    Auto 3

--- main.js
+++ main.js
@@ -38,11 +38,11 @@ mongoose.connect(connectionString, options);
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World2!');
+  res.send('Hello World!');
 });
 
 app.get('/health', function (req, res) {
-  res.send('All good2');
+  res.send('All good');
 });
 
 app.listen(3000, function () {
