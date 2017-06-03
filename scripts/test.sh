ba11f997cf4b7c0dbd388df1503be5099ec330ae
toannc <toan.nguyen-chi@lazada.com>
2017-06-03 20:07:38 +0700

    Improve testing Code

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,7 @@ ssh prod@ch.ilawyer.vn <<EOF
 ssh root@localhost -p 2210 
 echo "Testing code from Staging Branch"
 cd /opt/production
-git checkout Staging
-git pull
+git checkout production && git pull
 npm install
 cd /opt/production/node_modules/mocha
 ./bin/mocha /opt/prod/production/test/test.js
