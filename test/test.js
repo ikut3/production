355a0fe2050f24e5e5e17199176641cc8aeaf6de
toannc <toan.nguyen-chi@lazada.com>
2017-06-05 20:26:29 +0700

    Should be passed

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../main.js');
  
 describe('GET /', function() {
-    it('respond with This is a production system', function(done) {
-          request(app).get('/').expect('This is a production system', done);
+    it('respond with This is a production system2', function(done) {
+          request(app).get('/').expect('This is a production system2', done);
             });
 });
